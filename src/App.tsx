241b08493954f39f7b9c87d7104b94c42390596c
Tilman Möller <moeller.tilman@gmail.com>
2021-02-21 15:00:55 +0100

    message with avatar name text and hover

--- src/App.tsx
+++ src/App.tsx
@@ -23,9 +23,27 @@ const App: React.FC<Props> = () => {
         <div className="flex flex-row flex-auto bg-white rounded-tl-xl border shadow-xl">
           <div className="flex flex-col w-1/5">
             <div className="flex-none h-24 bg-red-200">top</div>
-            <div className="flex-auto overflow-y-auto">
+            <div className="flex-auto overflow-y-auto"> 
               {Array(5).fill(0).map((value, index) => 
-                <a key={index} className="block h-16 border-b p-2" href="/">{`Message ${index}`}</a>)}
+                <a key={index} className="block border-b" href="/">
+                  <div className={index === 1 ? "border-l-2 border-blue-500 bg-blue-100 border-transparent p-3 space-y-4": "p-3 space-y-4 hover:bg-gray-100"}>
+                    <div className="flex flex-row items-center space-x-2">
+                      <svg className="w-4 h-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                      </svg>
+                      <strong className="flex-grow text-sm">
+                        Name
+                      </strong>
+                      <div className="text-sm text-gray-500">5hr</div>
+                    </div>
+                    <div className="flex flex-row items-center space-x-1">
+                      <svg className="flex-none w-3 h-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                      </svg>
+                      <div className="flex-grow truncate text-sm">Some message content</div>
+                    </div>
+                  </div>
+                </a>)}
             </div>
           </div>
           <div className="w-3/5 border-l border-r border-gray-400">center</div>
