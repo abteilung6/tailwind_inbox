2eca98b22215d5a30a8789323b17af0365dd1587
Tilman Möller <moeller.tilman@gmail.com>
2021-02-21 12:42:22 +0100

    divide flex row in main sub and content

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div>hello</div>
-  );
+interface Props {
 }
+
+const App: React.FC<Props> = () => {
+return (
+  <div className="flex flex-row h-screen">
+      <div className="w-32 bg-red-200">MainMenu</div>
+      <div className="w-64 bg-green-200">Submenu</div>
+      <div className="flex-auto bg-blue-200">Content</div>
+  </div>
+);
+};
 
 export default App;
