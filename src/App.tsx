4be7dd8f65684982bee411f6b698b3ee797dc6bc
Tilman Möller <moeller.tilman@gmail.com>
2021-02-21 13:11:03 +0100

    main menu as col with circles

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,16 @@ interface Props {
 }
 
 const App: React.FC<Props> = () => {
-return (
-  <div className="flex flex-row h-screen">
-      <div className="w-32 bg-red-200">MainMenu</div>
-      <div className="w-64 bg-green-200">Submenu</div>
-      <div className="flex-auto bg-blue-200">Content</div>
-  </div>
-);
+  return (
+    <div className="flex flex-row h-screen">
+        <div className="flex flex-col justify-start items-center w-16 p-4 space-y-4 bg-gray-200">
+          {[1,2,3].map((value, index) => 
+            <div key={index} className="rounded-full bg-gray-400 w-8 h-8" />)}
+        </div>
+        <div className="w-64 bg-gray-100">Submenu</div>
+        <div className="flex-auto bg-blue-200">Content</div>
+    </div>
+  );
 };
 
 export default App;
