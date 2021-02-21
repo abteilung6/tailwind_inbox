61a70de37fb0069036607d52791d8ff632bf058c
Tilman Möller <moeller.tilman@gmail.com>
2021-02-21 13:34:16 +0100

    add inbox title with search

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,16 @@ const App: React.FC<Props> = () => {
           {[1,2,3].map((value, index) => 
             <div key={index} className="rounded-full bg-gray-400 w-8 h-8" />)}
         </div>
-        <div className="w-64 bg-gray-100">Submenu</div>
+        <div className="w-64 p-4 bg-gray-100 flex flex-col space-y-4">
+          <div className="flex flex-row justify-between items-center">
+            <h1 className="flex-auto font-semibold text-2xl">Inbox</h1>
+            <svg className="w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+              <path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd" />
+            </svg>
+          </div>
+          <div className="bg-red-200 h-54">New</div>
+          <div className="bg-red-200 h-54">New</div>
+        </div>
         <div className="flex-auto bg-blue-200">Content</div>
     </div>
   );
