8da4447b1adb63980aa0faa90131dbd9324f76b1
Tilman Möller <moeller.tilman@gmail.com>
2021-02-24 10:25:36 +0100

    add name and actions to the header of content

--- src/App.tsx
+++ src/App.tsx
@@ -47,7 +47,17 @@ const App: React.FC<Props> = () => {
             </div>
           </div>
           <div className="w-3/5 flex flex-col border-l border-r border-gray-400">
-            <div className="flex-none h-16 bg-red-200">a</div>
+            <div className="flex-none h-16 flex flex-row justify-between items-center p-4">
+              <div className="flex flex-col space-y-1">
+                <strong>Name</strong>
+                <input className="text-sm border-b border-dashed text-black placeholder-gray-600" placeholder="Add conversation title" />
+              </div>
+              <div>
+              <svg className="w-4 h-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z" />
+              </svg>
+              </div>
+            </div>
             <div className="flex-auto overflow-y-auto bg-green-200">a</div>
             <div className="flex-none h-40 bg-blue-200">a</div>
           </div>
