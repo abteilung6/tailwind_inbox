455d6b3810e44a9e5322f8d169eef5d9b60f0964
Tilman Möller <moeller.tilman@gmail.com>
2021-02-24 12:42:45 +0100

    add inbox icon

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,9 @@ const App: React.FC<Props> = () => {
   return (
     <div className="flex flex-row h-screen bg-gray-100">
         <div className="flex flex-col justify-start items-center flex-none w-16 p-4 space-y-4 bg-gray-200">
+          <svg className="w-8 h-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4" />
+          </svg>
           {[1,2,3].map((value, index) => 
             <div key={index} className="rounded-full bg-gray-400 w-8 h-8" />)}
         </div>
